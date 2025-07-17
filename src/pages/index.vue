7aca83cd4f9f33c9d7ad12e636e4a7d80e646c22
AleksandrTs <ascwork86@gmail.com>
2025-07-17 11:49:18 +0500

    add pinia

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,8 @@
 <template>
-  <div class="page-index">
-    <h1>Главная страница</h1>
-    <p>Это стартовая страница приложения.</p>
-  </div>
+  <h1>Главная страница</h1>
+  <p>Это стартовая страница приложения.</p>
 </template>
 
 <script></script>
 
-<style lang="scss">
-.page-index {
-  h1 {
-    color: #42b883;
-  }
-}
-</style>
+<style lang="scss"></style>
