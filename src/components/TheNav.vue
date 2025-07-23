2bbb1b2fa365b1f6868504a6eb4ce58903671587
AleksandrTs <ascwork86@gmail.com>
2025-07-23 11:55:55 +0500

    +

---
+++ src/components/TheNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="nav">asdsad</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  background-color: var(--lightSecondary);
+  padding: 15px;
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,33 @@
 <template>
   <router-link to="/log-in">login</router-link>
   <Card :cardOptions="cardTariff" />
+  <Card :cardOptions="cardTariff2" />
+  <Card :cardOptions="cardTariff3" />
+
+  <NavMenu />
 </template>
 
 <script>
 import Card from "../components/TheCard.vue";
+import NavMenu from "../components/TheNav.vue";
 export default {
   components: {
     Card,
+    NavMenu,
   },
   data() {
     return {
       cardTariff: {
         cardTtariff: "gold",
         tariff: "326 мест",
+      },
+      cardTariff2: {
+        cardTtariff: "silver",
+        tariff: "36 мест",
+      },
+      cardTariff3: {
+        cardTtariff: "black",
+        tariff: "32 мест",
       },
     };
   },
